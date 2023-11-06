fd4cbb1c59547a294608abbafdf191cc417bb80f
Per Thomas Haga <per.thomas.haga@agens.no>
2023-11-06 13:22:04 +0100

    Small updates

--- src/tests/writeSummary.test.ts
+++ src/tests/writeSummary.test.ts
@@ -1,5 +1,4 @@
 import writeSummary from "../utils/writeSummary";
-import * as fs from "fs";
 import * as core from "@actions/core";
 import {
   expect,
@@ -14,7 +13,7 @@ jest
   .spyOn(core.summary, "write")
   .mockReturnValue(Promise.resolve("test" as any));
 
-const mockXml = `<?xml version="1.0" encoding="UTF-8"?>
+const mockXmlFail = `<?xml version="1.0" encoding="UTF-8"?>
 <testsuites tests="1" failures="1">
     <testsuite name="ExampleTests" tests="1" failures="1" skipped="0" time="65.83285999298096">
         <properties>
@@ -27,8 +26,31 @@ const mockXml = `<?xml version="1.0" encoding="UTF-8"?>
     </testsuite>
 </testsuites>`;
 
+const mockXmlSuccess = `<?xml version="1.0" encoding="UTF-8"?>
+<testsuites name="jest tests" tests="4" failures="0" errors="0" time="5.004">
+  <testsuite name="uploadScreenshots" errors="0" failures="0" skipped="0" timestamp="2023-11-06T09:52:41" time="4.435" tests="2">
+    <testcase classname="uploadScreenshots throws an error if the Firebase service account is missing" name="uploadScreenshots throws an error if the Firebase service account is missing" time="0.015">
+    </testcase>
+    <testcase classname="uploadScreenshots successfully uploads screenshots and returns download URLs" name="uploadScreenshots successfully uploads screenshots and returns download URLs" time="0.011">
+    </testcase>
+  </testsuite>
+  <testsuite name="writeSummary" errors="0" failures="0" skipped="0" timestamp="2023-11-06T09:52:46" time="0.476" tests="2">
+    <testcase classname="writeSummary should write the summary correctly, no screenshots" name="writeSummary should write the summary correctly, no screenshots" time="0.012">
+    </testcase>
+    <testcase classname="writeSummary should write the summary correctly, with screenshots" name="writeSummary should write the summary correctly, with screenshots" time="0.003">
+    </testcase>
+  </testsuite>
+</testsuites>
+`;
+
 jest.mock("fs", () => ({
-  readFileSync: jest.fn().mockImplementation((_) => mockXml),
+  readFileSync: jest.fn().mockImplementation((path) => {
+    if ((path as string).includes("fail")) {
+      return mockXmlFail;
+    } else {
+      return mockXmlSuccess;
+    }
+  }),
   promises: {
     access: jest.fn().mockImplementation(() => Promise.resolve()),
   },
@@ -46,12 +68,12 @@ describe("writeSummary", () => {
     jest.clearAllMocks();
   });
 
-  it("should write the summary correctly, no screenshots", async () => {
-    await writeSummary("./src/tests/files/example.xml", []);
+  it("should write the summary correctly, failure, no screenshots", async () => {
+    await writeSummary("fail.xml", []);
     expect(core.summary).toMatchSnapshot();
   });
 
-  it("should write the summary correctly, with screenshots", async () => {
+  it("should write the summary correctly, failure, with screenshots", async () => {
     const screenshots: Screenshot[] = [
       {
         image: "ExampleUITests.testExample()",
@@ -59,7 +81,12 @@ describe("writeSummary", () => {
       },
     ];
 
-    await writeSummary("./src/tests/files/example.xml", screenshots);
+    await writeSummary("./fail.xml", screenshots);
+    expect(core.summary).toMatchSnapshot();
+  });
+
+  it("should write the summary correctly, success, no screenshots", async () => {
+    await writeSummary("success.xml", []);
     expect(core.summary).toMatchSnapshot();
   });
 });
