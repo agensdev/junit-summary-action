fced3041203952fef4e91b12bb3defa0de275e34
Per Thomas Haga <per.thomas.haga@agens.no>
2023-11-06 15:23:21 +0100

    Update

--- src/utils/addCommentToPR.ts
+++ src/utils/addCommentToPR.ts
@@ -1,21 +1,18 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import { context } from "@actions/github";
 
 export default async (result: WriteSummaryResult) => {
   try {
-    const token = core.getInput("github-token", { required: false });
-    core.debug(`TOKEN: ${token}`);
+    const token = core.getInput("github-token", { required: true });
     const octokit = github.getOctokit(token);
 
-    const context = github.context;
-    if (context.payload.pull_request == null) {
-      core.setFailed("No pull request found.");
-      return;
+    if (!context.payload.pull_request) {
+      throw new Error("No pull request found.");
     }
 
     const pull_request_number = context.payload.pull_request.number;
-    const owner = context.repo.owner;
-    const repo = context.repo.repo;
+    const { owner, repo } = context.repo;
 
     const comment = "This is a comment from the GitHub Action!";
     const response = await octokit.rest.issues.createComment({
