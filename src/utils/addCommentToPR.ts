938c14a5efa7352c01841f0f11b534506fe3b7ff
Per Thomas Haga <per.thomas.haga@agens.no>
2023-11-06 15:29:21 +0100

    Remove try catch

--- src/utils/addCommentToPR.ts
+++ src/utils/addCommentToPR.ts
@@ -3,27 +3,27 @@ import * as github from "@actions/github";
 import { context } from "@actions/github";
 
 export default async (result: WriteSummaryResult) => {
-  try {
-    const token = core.getInput("github-token", { required: true });
-    const octokit = github.getOctokit(token);
+  //try {
+  const token = core.getInput("github-token", { required: false });
+  const octokit = github.getOctokit(token);
 
-    if (!context.payload.pull_request) {
-      throw new Error("No pull request found.");
-    }
-
-    const pull_request_number = context.payload.pull_request.number;
-    const { owner, repo } = context.repo;
-
-    const comment = "This is a comment from the GitHub Action!";
-    const response = await octokit.rest.issues.createComment({
-      owner,
-      repo,
-      issue_number: pull_request_number,
-      body: comment,
-    });
-
-    core.info(`Comment created: ${response.data.html_url}`);
-  } catch (error) {
-    core.setFailed(`Action failed with error: ${error}`);
+  if (!context.payload.pull_request) {
+    throw new Error("No pull request found.");
   }
+
+  const pull_request_number = context.payload.pull_request.number;
+  const { owner, repo } = context.repo;
+
+  const comment = "This is a comment from the GitHub Action!";
+  const response = await octokit.rest.issues.createComment({
+    owner,
+    repo,
+    issue_number: pull_request_number,
+    body: comment,
+  });
+
+  core.info(`Comment created: ${response.data.html_url}`);
+  //} catch (error) {
+  //  core.setFailed(`Action failed with error: ${error}`);
+  //}
 };
