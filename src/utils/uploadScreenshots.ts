60897182292f21ba948e200804f4611f5497e956
Per Thomas Haga <per.thomas.haga@agens.no>
2023-11-07 15:45:46 +0100

    Use `-rf`

--- src/utils/uploadScreenshots.ts
+++ src/utils/uploadScreenshots.ts
@@ -76,7 +76,7 @@ async function getScreenshotsFromXcresult(
 ) {
   await exec("brew install imagemagick --quiet");
   await exec("brew install chargepoint/xcparse/xcparse --quiet");
-  await exec(`rm -ri ${destinationPath}`);
+  await exec(`rm -rf ${destinationPath}`);
   await exec(`xcparse screenshots --test ${xcresultPath} ${destinationPath}`);
   await exec(`${__dirname}/scripts/extract_screenshots.sh ${destinationPath}`);
 }
