9f7d29e291fb60bd5b1034502fbdb02ce3e61b08
Per Thomas Haga <per.thomas.haga@agens.no>
2024-04-24 20:55:41 +0200

    Add option for failing if tests fail

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,13 @@ let githubToken: string | undefined = core.getInput("github-token", {
   required: false,
 });
 
+let failIfTestsFail: boolean | undefined = core.getBooleanInput(
+  "fail-if-tests-fail",
+  {
+    required: false,
+  }
+);
+
 if (xcresultPath === "") {
   xcresultPath = undefined;
 }
@@ -50,6 +57,9 @@ try {
   const result = await writeSummary(path, screenshots);
   if (githubToken) {
     await addCommentToPR(result);
+  }
+  if (failIfTestsFail && result.numberOfFailedTests > 0) {
+    core.setFailed("Tests failed.");
   }
 } catch (error) {
   let message = "An unknown error occured.";
